f6763b881e7556d433c5234b80833d24b13df1b4
Avinash Rijal <avinashrijal@gmail.com>
2022-05-19 08:22:54 +0545

    show in tabular format with prettytable

--- crawler.py
+++ crawler.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+from prettytable import PrettyTable
 
 URL = "https://localelection.ekantipur.com/pradesh-3/district-kathmandu/kathmandu?lng=eng"
 r = requests.get(URL)
 
 soup = BeautifulSoup(r.content, 'html5lib')
 
-results = []
+t = PrettyTable(['Name', 'Party', 'Vote Count'])
 
 nominee_list_group = soup.find('div', attrs = { 'class': 'nominee-list-group' })
 
@@ -17,10 +18,9 @@ table = first_column.find_all('div', attrs = { 'class': 'candidate-list' })
 for row in table:
   name = row.select_one('.candidate-name').text
   if name == 'Balendra Shah' or name == 'Keshav Sthapit' or name == 'Shirjana Shrestha':
-    result = {}
-    result['name'] = row.select_one('.candidate-name').text
-    result['party_name'] = row.select_one('.candidate-party-name').text
-    result['vote-numbers'] = row.select_one('.vote-numbers').text
-    results.append(result)
+    name = row.select_one('.candidate-name').text
+    party_name = row.select_one('.candidate-party-name').text
+    vote_count = row.select_one('.vote-numbers').text
+    t.add_row([name, party_name, vote_count])
 
-print(results)
+print(t)
